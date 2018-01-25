af9c7505d9caeeba3a28d77bd4e30866cddc3507
Langston Barrett <langston.barrett@gmail.com>
2018-01-25 07:06:14 +0000

    fix nix build

--- default.nix
+++ default.nix
@@ -9,6 +9,7 @@ with pkgs; stdenv.mkDerivation rec {
 
   buildInputs = [
     git
+    glibcLocales
     haskellPackages.Agda
     # to compile agda files, we need a few more haskell packages
     (haskellPackages.ghcWithPackages (pkgs: with pkgs; [ieee text]))
@@ -18,20 +19,29 @@ with pkgs; stdenv.mkDerivation rec {
     for project in "agda/agda-stdlib" "gallais/agdARGS" "gallais/agdarsec"; do
       git clone "https://github.com/$project" || true
     done
-    # For some reason, there is an error that doesn't happen when called
-    # manually: <stdout>: commitBuffer: invalid argument (invalid character)
+
+    # See https://github.com/agda/agda/issues/2922
+    export LANG=en_US.UTF-8
+    export LC_CTYPE=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
+
     agda --library-file=./libraries \
          --library=standard-library \
          --library=agdarsec \
          --library=agdARGS \
          --compile-dir=build \
          -i "$PWD" \
-         -c Main.agda || true
+         -c Main.agda
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp build/Main $out/bin/agda-scheme
   '';
 
   meta = {
     homepage = https://github.com/siddharthist/write-yourself-a-scheme-agda;
-    description = "";
+    description = "Like 'Write Yourself a Scheme in 48 Hours, but in Agda!";
     license = stdenv.lib.licenses.mpl2;
     platforms = stdenv.lib.platforms.unix;
     maintainers = with stdenv.lib.maintainers; [ siddharthist ];
